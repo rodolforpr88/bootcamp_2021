fe6999d6ed06da187c8229026a787715e442740a
rodolforpr88 <rodolforpr88@gmail.com>
2021-04-29 16:20:53 -0300

    class 33 - If Statement - 05min

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-// console.log(3 + 4);
-// console.error('OH NOO!!!!');
-console.log(3 + 4, 'hello', true);
+// if (1 === 1) {
+//   console.log("It's True!");
+// }
+
+// if (1 !== 1) {
+//   console.log("It's True!");
+// }
+
+let rating = 3;
+
+if (rating === 3) {
+  console.log("You're a superstar!");
+}
+
+let num = 37;
+
+if (num % 2 !== 0) {
+  console.log("ODD NUMBER!");
+}
