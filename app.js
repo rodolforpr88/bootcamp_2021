b0c85c4e344e8e836a06cf1030d2924a21412496
rodolforpr88 <rodolforpr88@gmail.com>
2021-05-01 10:08:49 -0300

    class 42 - Switch Statement - 08min

--- app.js
+++ app.js
@@ -1,6 +1,61 @@
-let x = 7;
-// Order
-// '!' -> && -> || unless u use '()'
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
-x == 7 || x === 3 && x > 10;
-(x == 7 || x === 3) && x > 10;
+// let day = 1;
+
+// if (day === 1) {
+//   console.log("MONDAY");
+// } else if (day === 2) {
+//   console.log("TUESDAY");
+// } else if (day === 3) {
+//   console.log("WEDNESDAY");
+// } else if (day === 4) {
+//   console.log("THURSDAY");
+// } else if (day === 5) {
+//   console.log("FRIDAY");
+// } else if (day === 6) {
+//   console.log("SATURDAY");
+// } else if (day === 7) {
+//   console.log("SUNDAY");
+// } else {
+//   console.log("INVALID DAY");
+// }
+
+// switch (day) {
+//   case 1:
+//     console.log("MONDAY");
+//     break;
+//   case 2:
+//     console.log("TUESDAY");
+//     break;
+//   case 3:
+//     console.log("WEDNESDAY");
+//     break;
+//   case 4:
+//     console.log("THURSDAY");
+//     break;
+//   case 5:
+//     console.log("FRIDAY");
+//     break;
+//   case 6:
+//     console.log("SATURDAY");
+//     break;
+//   case 7:
+//     console.log("SUNDAY");
+//     break;
+//   default:
+//     console.log("INVALID DAY");
+// }
+
+let emoji = 'sad face'
+
+switch (emoji) {
+  case 'sad face':
+  case 'happy face':
+    console.log('yellow');
+    break;
+  case 'eggplant':
+    console.log('purple');
+    break;
+  case 'lips':
+  case 'heart':
+    console.log('red');
+    break;
+}
