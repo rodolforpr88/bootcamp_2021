8a7cc45b3cbdb1fcf1c8262b7b357a5a52876abd
rodolforpr88 <rodolforpr88@gmail.com>
2021-05-03 11:25:52 -0300

    class 51 - Reverse and Join - 03min

--- app.js
+++ app.js
@@ -7,15 +7,7 @@ let ingredients = [
   'shrimp'
 ]
 
-console.log(ingredients.includes('water'));
-console.log(ingredients.includes('fire'));
+console.log(ingredients);
+console.log(ingredients.reverse());
 
-console.log(ingredients.includes('water', 3));
-console.log(ingredients.includes('water', 0));
-
-if (ingredients.includes('flour')) {
-  console.log('TRUE');
-}
-
-console.log(ingredients.indexOf('flour'));
-console.log(ingredients.indexOf('other'));
+console.log(ingredients.join('-'));
