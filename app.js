0fbde9fbf68b71104919413580d82a41efb1dd6e
rodolforpr88 <rodolforpr88@gmail.com>
2021-05-06 10:54:24 -0300

    class 77 - Our First Function

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const animalPairs = [
-  ['doe', ['buck', 'stag']],
-  ['ewe', 'ram'],
-  ['peahen', 'peacok']
-];
+// function declaration or function keywork
+// function name(params) {
+//   // do something
+// }
 
-animalPairs[0][0]
-animalPairs[0][1]
-
-animalPairs[1][0]
-animalPairs[1][1]
-
-animalPairs[2][0]
-animalPairs[2][1]
-
-animalPairs[0][1][1]
+function grumpus() {
+  console.log('something...');
+  console.log('something again...');
+  console.log('something again again!...');
+}
+grumpus();
