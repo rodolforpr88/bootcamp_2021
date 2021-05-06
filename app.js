f3e89d2e344d9cff6b47e86350fe5e32945af200
rodolforpr88 <rodolforpr88@gmail.com>
2021-05-06 19:41:43 -0300

    class 86 - Function Challange4: Get Playing Card

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// function isPangram(sentence) {
-//   let lowerCase = sentence.toLowerCase();
-//   for (let char of 'abcdefghijklmnopqrstuvwxyz') {
-//     if (lowerCase.indexOf(char) === -1) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-function isPangram(sentence) {
-  let lowerCase = sentence.toLowerCase();
-  for (let char of 'abcdefghijklmnopqrstuvwxyz') {
-    if (lowerCase.includes(char)) {
-      return false;
-    }
-  }
-  return true;
+function getCard(params) {
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suits = ['clubs', 'hearts', 'spades', 'diamond'];
+  sortedValue = Math.round((Math.random()) * values.length) - 1;
+  sortedSuit = Math.round((Math.random()) * suits.length) - 1;
+  let result = {
+    value: values[sortedValue],
+    suit: suits[sortedSuit]
+  };
+  return result;
 }
+// solution created the function 'pick' separeted
