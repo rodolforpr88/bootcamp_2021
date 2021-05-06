33f25cf886081388dd1b60326f24037eff4c8e78
rodolforpr88 <rodolforpr88@gmail.com>
2021-05-06 11:45:47 -0300

    class 82 - More on Return Values

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-function add(x, y) {
-  // return [x, y]
-  return x + y;
+// function square(x) {
+//   return x * x;
+// }
+
+// function isPurple(color) {
+//   if (color.toLowerCase() == 'purple') {
+//     return true;
+//     console.log('Not going to run!');
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+// function isPurple(color) {
+//   if (color.toLowerCase() == 'purple') {
+//     return true;
+//   }
+//   return false;
+// }
+
+// function isPurple(color) {
+//   return color.toLowerCase() === 'purple';
+// }
+
+function containsPurple(arr) {
+  for (let color of arr) {
+    if (color === 'purple' || color === 'magenta') {
+      return true;
+    }
+  }
+  return false;
 }
