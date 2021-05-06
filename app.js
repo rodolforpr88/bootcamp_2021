e8378c04eb0be7c50b3053ff18af71c9a9ad4483
rodolforpr88 <rodolforpr88@gmail.com>
2021-05-06 19:02:53 -0300

    class 85 - Function Challange3: Pangrams

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// Wrote a function to find the avg value in an array numbers
+// function isPangram(sentence) {
+//   let lowerCase = sentence.toLowerCase();
+//   for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+//     if (lowerCase.indexOf(char) === -1) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-function avg(arr) {
-  let total = 0;
-  for (const num of arr) total += num;
-  return total / arr.length
+function isPangram(sentence) {
+  let lowerCase = sentence.toLowerCase();
+  for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+    if (lowerCase.includes(char)) {
+      return false;
+    }
+  }
+  return true;
 }
-
-// console out
-// avg([0,50])
-// 25
-// avg([75,76,80,95,100])
-// 85.2
